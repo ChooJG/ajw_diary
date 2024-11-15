1ff3c1150b231e0a8b5b692f210fe1c809da25f0
ChooJG <cnwprms@naver.com>
2024-11-15 22:47:20 +0900

    feat: urls.py 작성 및 템플릿 파일 생성

--- diary/urls.py
+++ diary/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('/list', views.list, name="diary-list"),
+    path('', views.index, name="index"),                    # 시작 화면
+    path('/list', views.list, name="diary-list"),           # 일기 목록 조회
+    path("/read", views.read, name="diary-list"),           # 일기 조회
+    path('/create', views.create, name="diary-create"),     # 일기 작성
+    path('/update', views.update, name="diary-update"),     # 수정
+    path('/delete', views.delete, name="diary-delete")      # 삭제
 
 ]
--- diary/views.py
+++ diary/views.py
@@ -11,3 +11,23 @@ def index(request):
 def list(request):
     context = {}
     return render(request, 'diary/list.html', context)
+
+
+def read(request):
+    context = {}
+    return render(request, 'diary/read.html', context)
+
+
+def create(request):
+    context = {}
+    return render(request, 'diary/create.html', context)
+
+
+def update(request):
+    context = {}
+    return render(request, 'diary/update.html', context)
+
+
+def delete(request):
+    context = {}
+    return render(request, 'diary/delete.html', context)
