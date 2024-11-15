5dc9cb6ff53e6be136e28a6f278b7dbb025c4bf9
ChooJG <cnwprms@naver.com>
2024-11-15 23:07:52 +0900

    fix: 일기 작성 중 이미지 저장 안되는 오류 있음

--- diary/urls.py
+++ diary/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),                    # 시작 화면
-    path('/list', views.list, name="diary-list"),           # 일기 목록 조회
-    path("/read", views.read, name="diary-list"),           # 일기 조회
-    path('/create', views.create, name="diary-create"),     # 일기 작성
-    path('/update', views.update, name="diary-update"),     # 수정
-    path('/delete', views.delete, name="diary-delete")      # 삭제
+    path('list', views.list, name="diary-list"),           # 일기 목록 조회
+    path("read", views.read, name="diary-read"),           # 일기 조회
+    path('create', views.create, name="diary-create"),     # 일기 작성
+    path('update', views.update, name="diary-update"),     # 수정
+    path('delete', views.delete, name="diary-delete")      # 삭제
 
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DiaryForm, DiaryImageFormSet
+
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Diary, DiaryImage
 
 # Create your views here.
 
@@ -9,17 +14,59 @@ def index(request):
 
 
 def list(request):
-    context = {}
+    # 최신 일기가 먼저 보이도록 정렬
+    diary_list = Diary.objects.all().order_by('-diary_date')
+
+    # 페이지네이션 (한 페이지당 10개의 일기)
+    paginator = Paginator(diary_list, 10)
+    page = request.GET.get('page', 1)
+    diaries = paginator.get_page(page)
+
+    context = {
+        'diaries': diaries,
+    }
     return render(request, 'diary/list.html', context)
 
 
 def read(request):
-    context = {}
+    # URL 파라미터로 전달된 일기 ID
+    diary_id = request.GET.get('id')
+
+    # 해당 ID의 일기가 없으면 404 에러
+    diary = get_object_or_404(Diary, id=diary_id)
+
+    # 일기에 연결된 이미지들 가져오기
+    images = diary.images.all()
+
+    context = {
+        'diary': diary,
+        'images': images,
+    }
     return render(request, 'diary/read.html', context)
 
 
 def create(request):
-    context = {}
+    if request.method == 'POST':
+        form = DiaryForm(request.POST)
+        formset = DiaryImageFormSet(request.POST, prefix='images')
+
+        if form.is_valid():
+            # 먼저 일기를 저장
+            diary = form.save()
+
+            # 저장된 일기를 formset의 instance로 설정하고 저장
+            formset.instance = diary
+            if formset.is_valid():
+                formset.save()
+                return redirect('diary-read', pk=diary.pk)
+    else:
+        form = DiaryForm()
+        formset = DiaryImageFormSet(prefix='images')
+
+    context = {
+        'form': form,
+        'formset': formset
+    }
     return render(request, 'diary/create.html', context)
 
 
